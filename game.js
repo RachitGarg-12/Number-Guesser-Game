31c9c24124de746147b84eeb5c95e591ee83429a
Rachit <rachitgarg146@gmail.com>
2022-10-01 11:58:15 +0530

    changing max score to 100

--- game.js
+++ game.js
@@ -1,17 +1,18 @@
 let x = Math.floor((Math.random() * 100) + 1);
-let a;let num=0;
-do{
-  a=prompt("Guess the number between 1 to 100:")
-  a=Number.parseInt(a);num++;
-  if(a==x){
-    console,log("Your Guess is right.");
-    break;}
-  else if(a<x){
+let a; let num = 0;
+do {
+  a = prompt("Guess the number between 1 to 100:")
+  a = Number.parseInt(a); num++;
+  if (a == x) {
+    console, log("Your Guess is right.");
+    break;
+  }
+  else if (a < x) {
     console.log("You have guessed a smaller number.");
   }
-  else{
-     console.log("You have guessed a greater number.");
+  else {
+    console.log("You have guessed a greater number.");
   }
-}while(true)
+} while (true)
 
-console.log(100-num+1);
+console.log((100 + 1) - num);
