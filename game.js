e7fb46a82f87a3c3c28bd60759e4db23ee8852ab
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-10-01 11:41:13 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,17 @@
+let x = Math.floor((Math.random() * 100) + 1);
+let a;let num=0;
+do{
+  a=prompt("Guess the number between 1 to 100:")
+  a=Number.parseInt(a);num++;
+  if(a==x){
+    console,log("Your Guess is right.");
+    break;}
+  else if(a<x){
+    console.log("You have guessed a smaller number.");
+  }
+  else{
+     console.log("You have guessed a greater number.");
+  }
+}while(true)
+
+console.log(100-num);
